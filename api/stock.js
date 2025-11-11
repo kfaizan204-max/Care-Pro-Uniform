c94270c82eb1e55a1e44e6ee65818e30c0aa801c
kfaizan204-max <kfaizan204@gmail.com>
2025-11-11 17:15:17 +0300

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -7,11 +7,13 @@ export default async function handler(req, res) {
   if(req.method === 'GET') {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
     res.status(200).json(data);
-  } else if(req.method === 'POST') {
+  } 
+  else if(req.method === 'POST') {
     const body = req.body;
     fs.writeFileSync(filePath, JSON.stringify(body, null, 2));
     res.status(200).json({ success: true });
-  } else {
+  } 
+  else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
